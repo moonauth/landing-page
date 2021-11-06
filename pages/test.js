78080d93416daad9317b7e36ad7c30d4539655aa
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-11-06 21:55:54 +0100

    Responsive testing

--- pages/test.js
+++ pages/test.js
@@ -49,26 +49,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.svg" />
       </Head>
 
-      <main className="bg-moonwhite md:h-screen h-full">
+      <main className="bg-moonwhite h-screen">
         <div className="w-full flex flex-wrap h-full">
 
 			    <div className= "md:pt-0 w-full md:flex-1 md:order-last">
 			    	<img src="/hand.jpg" className="w-full h-64 md:h-full object-cover" />
 			    </div>
-
-			    <div className="w-full md:p-36 md:w-6/12 flex-1 justify-center items-center relative text-moonblack m-20">
-			    	<div className="relative md:p-2">
+          <div className= "md:pt-0 w-full flex-1 md:order-first">
+            <div className="relative md:p-2">
               <div className="w-36 pb-10">
                 <img src="/moonauth.svg"></img>
               </div>
 			    		  <h1 className="text-5xl md:text-7xl mb-10 leading-1 font-dm_serif">Passwords are about to become <h1 className="text-transparent bg-clip-text bg-gradient-to-tl from-g_blue to-g_orange">obsolete.</h1></h1>
 			    		  <h2 className="text-2xl mb-1 font-dm_sans">We are still in development</h2>
                 <h2 className="text-2xl mb-4 font-dm_sans">Join us and be part of this journey!</h2>
-              <div className="w-full flex-1 mt-16 font-dm_sans font-medium md:w-11/12 md:visible invisible">
-                <a onClick={() => handle("discord")} className="bg-moonwhite_dark hover:bg-moonwhite_hover transition-all text-moonblack p-3 rounded-2xl px-5 mr-5" ><img src="/social/discord.svg" className="w-5 inline-flex mr-3 fill-current opacity-70"></img>Discord</a>
-                <a onClick={() => handle("slack")} className="bg-moonwhite_dark hover:bg-moonwhite_hover transition-all text-moonblack p-3 rounded-2xl px-5 mr-5" ><img src="/social/slack.svg" className="w-5 inline-flex mr-3 fill-current opacity-70"></img>Slack</a>
-                <a onClick={() => handle("github")} className="bg-moonwhite_dark hover:bg-moonwhite_hover transition-all text-moonblack p-3 rounded-2xl px-5 mr-5" ><img src="/social/twitter.svg" className="w-5 inline-flex mr-3 fill-current opacity-70"></img>Twitter</a>
-                <a onClick={() => handle("twitter")} className="bg-moonwhite_dark hover:bg-moonwhite_hover transition-all text-moonblack p-3 rounded-2xl px-5 mr-5" ><img src="/social/github.svg" className="w-5 inline-flex mr-3 fill-current opacity-70"></img>GitHub</a>
+              <div className="w-full flex mt-16 font-dm_sans font-medium md:w-11/12">
+                <a onClick={() => handle("discord")} className="bg-moonwhite_dark hover:bg-moonwhite_hover transition-all text-moonblack p-3 rounded-2xl px-5 mr-5" ><img src="/social/discord.svg" className="w-5 inline-block mr-3 fill-current opacity-70"></img>Discord</a>
+                <a onClick={() => handle("slack")} className="bg-moonwhite_dark hover:bg-moonwhite_hover transition-all text-moonblack p-3 rounded-2xl px-5 mr-5" ><img src="/social/slack.svg" className="w-5 inline-block mr-3 fill-current opacity-70"></img>Slack</a>
+                <a onClick={() => handle("github")} className="bg-moonwhite_dark hover:bg-moonwhite_hover transition-all text-moonblack p-3 rounded-2xl px-5 mr-5" ><img src="/social/twitter.svg" className="w-5 inline-block mr-3 fill-current opacity-70"></img>Twitter</a>
+                <a onClick={() => handle("twitter")} className="bg-moonwhite_dark hover:bg-moonwhite_hover transition-all text-moonblack p-3 rounded-2xl px-5 mr-5" ><img src="/social/github.svg" className="w-5 inline-block mr-3 fill-current opacity-70"></img>GitHub</a>
               </div>
 			    	</div>
 			    </div>
